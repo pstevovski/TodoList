1d591fbbaf2e5a3bc5189701d130e98a379e3269
Petar Stevovski <stevovski.petar@gmail.com>
2018-09-04 02:10:16 +0200

    Added option to delete all list items.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
     todo: [],
     completed: []
  }
+
 // Declaring SVGs for the complete / remove buttons.
 const removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
 const completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
@@ -10,14 +11,16 @@ const completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns
 // Render the lists on page load.
 renderList();
 
-// Notification test.
+// Notification.
 const notification = document.querySelector(".notification");
+
+
+
 // Item input.
 document.getElementById("addItem").addEventListener("click", ()=>{
     const value = document.getElementById("itemInput").value;
     // If input field is NOT empty, run the function.
     if(value){
-        //addToList(value);
         add(value);
         document.getElementById("itemInput").value = "";
     }
@@ -27,11 +30,9 @@ document.getElementById("itemInput").addEventListener("keydown", function(e){
     const value = this.value;
     // If the key code matches Enter or NumpadEnter AND there is some value, run the function.
     if((e.code === "Enter" || e.code === "NumpadEnter") && value){
-        //addToList(value);
         add(value);
         // Empty input field.
         this.value = "";
-        console.log(data);
     }
 })
 function add(value){
@@ -49,13 +50,19 @@ function add(value){
         notification.innerHTML = "";
     }, 1500);
     console.log(data);
+    deleteAll.style.display = "block";
 }
 // Save the data object into local storage.
 function dataObjectUpdate(){
     localStorage.setItem("todoList", JSON.stringify(data));
 }
-
 function renderList(){
+    const deleteAll = document.querySelector("#deleteAllToBe");
+    if(data.todo.length >= 1 || data.completed.length >= 1 ){
+        deleteAll.style.display = "block";
+    } else {
+        deleteAll.style.display = "none";
+    }
     // If the length of the arrays is 0, return (exit) the function.
     if(!data.todo.length && !data.completed.length) return;
 
@@ -67,6 +74,7 @@ function renderList(){
         const value = data.completed[j];
         addToList(value, completed);
     }
+    
 }
 
 function removeItem(){
@@ -74,14 +82,14 @@ function removeItem(){
     const parent = item.parentNode;
     const id = parent.id;
     const value = item.innerText;
-
+    
     if(id === "todo"){
         data.todo.splice(data.todo.indexOf(value), 1);
     } else {
         data.completed.splice(data.completed.indexOf(value), 1);
     }
     
-    // Notification test.
+    // Notification.
     notification.innerHTML = "<p>Task deleted !</p>";
     notification.classList.add("notification");
     notification.classList.add("notificationDeleted");
@@ -90,11 +98,11 @@ function removeItem(){
         notification.classList.remove("notificationDeleted");
         notification.innerHTML = "";
     }, 1500);
+    dataObjectUpdate();
 
     console.log(data);
     // Remove the item from the list(parent).
     parent.removeChild(item);
-    dataObjectUpdate();
 }
 function completeItem(){
     const item = this.parentNode.parentNode;
@@ -134,7 +142,7 @@ function addToList(value, completed){
     // Create a list item.
     const item = document.createElement("li");
     item.innerText = value;
-
+    
     // Create buttons.
     const buttons = document.createElement("div");
     buttons.classList.add("buttons");
@@ -158,5 +166,11 @@ function addToList(value, completed){
     item.appendChild(buttons);
     // Insert each new item in the list, infront of the previous one.
     list.insertBefore(item, list.childNodes[0]);
-
 }
+
+// Delete all list items.
+const deleteAll = document.querySelector("#deleteAllToBe");
+deleteAll.addEventListener("click", ()=>{
+    localStorage.removeItem("todoList");
+    location.reload();
+})
